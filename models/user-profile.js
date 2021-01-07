9d0323a3d0dc4853b47ef249ba9a9544789faf31
Vinaygarg82 <56172081+Vinaygarg82@users.noreply.github.com>
2021-01-07 06:08:00 -0600

    Delete user-profile.js

--- models/user-profile.js
+++
@@ -1,9 +0,0 @@
-﻿class UserProfile {
-    constructor(cnf) {
-        this.mobileNumber = cnf.mobileNumber,
-            this.firstName = cnf.firstName,
-            this.lastName = cnf.lastName;
-    }
-}
-
-export default UserProfile;
